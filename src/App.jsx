d16bbed433deb78e2eff044d18330ac0062415c3
nbaulib <aabautista70@gmail.com>
2024-03-08 23:52:03 -0500

    Apple

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Card from './Card';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const cardPairs = [
+    { question: 'Which apple was named after an Australian?', answer: 'Granny Smith' },
+    { question: 'Which apple was trademarked by Cornell scientists?', answer: 'SnapDragon' },
+    { question: 'Old norse?', answer: 'Crabapple' },
+  ];
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1>Apple</h1>
+      <h2>Learn about the origin of apple names!</h2>
+      <p>total number of cards: {cardPairs.length}</p>
+      <Card cardPairs={cardPairs} />
+    </div>
   )
 }
 
---
+++ src/Card.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Card = ({ cardPairs }) => {
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [showQuestion, setShowQuestion] = useState(true);
+
+  const handleNextClick = () => {
+    const newIndex = Math.floor(Math.random() * cardPairs.length);
+    setCurrentCardIndex(newIndex);
+    setShowQuestion(true); // Reset to show question when next card is displayed
+  };
+
+  const handleClick = () => {
+    setShowQuestion(!showQuestion);
+  };
+
+  return (
+    <div className="card-set">
+      <button onClick={handleNextClick}>Next</button>
+      <div className="card" onClick={handleClick}>
+        <div className="card-content">
+          {showQuestion ? <p>{cardPairs[currentCardIndex].question}</p> : <p>{cardPairs[currentCardIndex].answer}</p>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
